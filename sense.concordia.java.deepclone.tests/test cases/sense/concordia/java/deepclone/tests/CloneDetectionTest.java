f369570e2439b2fe5f999856e4b453bc1c2b8ab0
Yiming Tang <t_yiming@encs.concordia.ca>
2021-10-28 19:06:43 -0400

    fix path for testing

--- sense.concordia.java.deepclone.tests/test cases/sense/concordia/java/deepclone/tests/CloneDetectionTest.java
+++ sense.concordia.java.deepclone.tests/test cases/sense/concordia/java/deepclone/tests/CloneDetectionTest.java
@@ -1,80 +1,52 @@
 package sense.concordia.java.deepclone.tests;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import org.junit.Rule;
+import org.junit.Test;
 
+import java.util.Set;
+import java.util.EnumSet;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.charset.Charset;
 import java.util.Collections;
-import java.util.EnumSet;
-import java.util.Set;
+
 import java.util.stream.Collectors;
 
-import javax.tools.JavaCompiler;
-import javax.tools.ToolProvider;
-
-import org.junit.Test;
-
-import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.jdt.core.ICompilationUnit;
-import org.eclipse.jdt.core.IPackageFragment;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTParser;
-import org.eclipse.jdt.ui.tests.refactoring.GenericRefactoringTest;
-import org.eclipse.jdt.ui.tests.refactoring.rules.RefactoringTestSetup;
 
 import sense.concordia.java.deepclone.core.detectors.JavaDeepCloneDetector;
 import sense.concordia.java.deepclone.core.detectors.JavaDeepCloneResult;
 import sense.concordia.java.deepclone.core.detectors.JavaDeepCloneType;
 
+import static org.junit.Assert.*;
+
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.ui.tests.refactoring.GenericRefactoringTest;
+import org.eclipse.jdt.ui.tests.refactoring.infra.RefactoringTestPlugin;
+import org.eclipse.jdt.ui.tests.refactoring.rules.RefactoringTestSetup;
+
 @SuppressWarnings("restriction")
 public class CloneDetectionTest extends GenericRefactoringTest {
-	
+
 	private static final String REFACTORING_PATH = "JavaDeepClone/";
-	
-	@Override
-	public String getRefactoringPath() {
-		return REFACTORING_PATH;
-	}
-	
+	private static final String RESOURCE_PATH = "resources";
+
 	public CloneDetectionTest() {
-		rts= new RefactoringTestSetup();
+		rts = new RefactoringTestSetup();
 	}
 	
-	/**
-	 * Compile the test case
-	 */
-	private static boolean compiles(String source, Path path) throws IOException {
-		// Save source in .java file.
-		File sourceFile = new File(path.toFile(), "bin/p/A.java");
-		sourceFile.getParentFile().mkdirs();
-		Files.write(sourceFile.toPath(), source.getBytes());
-
-		// Compile source file.
-		JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-		boolean compileSuccess = compiler.run(null, null, null, sourceFile.getPath()) == 0;
-
-		sourceFile.delete();
-		return compileSuccess;
+	protected CloneDetectionTest(RefactoringTestSetup rts) {
+		this.rts= rts;
 	}
-	
-	@Override
-	protected ICompilationUnit createCUfromTestFile(IPackageFragment pack, String cuName) throws Exception {
 
-		ICompilationUnit unit = super.createCUfromTestFile(pack, cuName);
-		
-		if (!unit.isStructureKnown())
-			throw new IllegalArgumentException(cuName + " has structural errors.");
-
-		Path directory = Paths.get(unit.getParent().getParent().getParent().getResource().getLocation().toString());
-
-		assertTrue("Should compile the testing cases:", compiles(unit.getSource(), directory));
-
-		return unit;
+	protected String getRefactoringPath() {
+		return REFACTORING_PATH;
 	}
 
 	/**
@@ -84,8 +56,7 @@ public class CloneDetectionTest extends GenericRefactoringTest {
 	 * @throws Exception
 	 */
 	private void helper(CloneDetectionExpectedResult... expectedResults) throws Exception {
-		// compute the actual results.
-		ICompilationUnit cu = this.createCUfromTestFile(this.getPackageP(), "A");
+		ICompilationUnit cu = createCUfromTestFile(getPackageP(), "A");
 
 		ASTParser parser = ASTParser.newParser(AST.JLS15);
 		parser.setResolveBindings(true);
@@ -116,6 +87,22 @@ public class CloneDetectionTest extends GenericRefactoringTest {
 	public void testCloneableInterface() throws Exception {
 		this.helper(new CloneDetectionExpectedResult(EnumSet.of(JavaDeepCloneType.CLONE_METHOD),
 				Collections.singleton("")));
+	}
+	
+	/*
+	 * This method could fix the issue that the bundle has no entry.
+	 */
+	@Override
+	public String getFileContents(String fileName) throws IOException {
+		Path absolutePath = getAbsolutePath(fileName);
+		byte[] encoded = Files.readAllBytes(absolutePath);
+		return new String(encoded, Charset.defaultCharset());
+	}
+
+	private static Path getAbsolutePath(String fileName) {
+		Path path = Paths.get(RESOURCE_PATH, fileName);
+		Path absolutePath = path.toAbsolutePath();
+		return absolutePath;
 	}
 
 }
